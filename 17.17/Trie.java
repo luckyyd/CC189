b6d8751e67a6cf02cc2271816e06e2ccf07a7699
luckyyd <luckyyd@tju.edu.cn>
2016-06-24 10:28:41 +0800

    17.17

---
+++ 17.17/Trie.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+
+/* Implements a trie. We store the input list of words in tries so
+ * that we can efficiently find words with a given prefix. 
+ */ 
+public class Trie
+{
+    // The root of this trie.
+    private TrieNode root;
+
+    public Trie(String s){
+    	insertString(s, 0);
+    }
+    
+    public Trie(){}
+    
+    /* Takes a list of strings as an argument, and constructs a trie that stores these strings. */
+    public Trie(ArrayList<String> list) {
+        root = new TrieNode();
+        for (String word : list) {
+            root.addWord(word);
+        }
+    }  
+
+    /* Takes a list of strings as an argument, and constructs a trie that stores these strings. */    
+    public Trie(String[] list) {
+        root = new TrieNode();
+        for (String word : list) {
+            root.addWord(word);
+        }
+    }    
+
+    /* Checks whether this trie contains a string with the prefix passed
+     * in as argument.
+     */
+    public boolean contains(String prefix, boolean exact) {
+        TrieNode lastNode = root;
+        int i = 0;
+        for (i = 0; i < prefix.length(); i++) {
+            lastNode = lastNode.getChild(prefix.charAt(i));
+            if (lastNode == null) {
+                return false;	 
+            }
+        }
+        return !exact || lastNode.terminates();
+    }
+    
+    public boolean contains(String prefix) {
+    	return contains(prefix, false);
+    }
+    
+    public TrieNode getRoot() {
+    	return root;
+    }
+
+
+	public void insertString(String s, int i) {
+		// TODO Auto-generated method stub
+		
+	}
+}
